a090992b9bf7cc509bb7eb44600e3439ef90ffc1
Hao Nguyen <haonv@paxcreation.com>
2021-08-28 17:35:23 +0700

    config

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Moola X',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
